b6999102ad2433574404fb1bab77e2ea2e19d834
Janek <45148583+operator-paramedyk@users.noreply.github.com>
2022-06-22 00:25:16 +0200

    ship added, not displayed yet

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store all the settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize game's settings."""
+
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (221, 183, 123)
