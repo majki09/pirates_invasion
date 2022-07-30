de6961b52145ed94f9b7f7ef3fd181e74bbda5e4
Janek <45148583+operator-paramedyk@users.noreply.github.com>
2022-07-31 00:43:55 +0200

    level added

--- alien_invasion.py
+++ alien_invasion.py
@@ -83,6 +83,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # Delete remaining aliens and bullets.
             self.aliens.empty()
@@ -94,7 +95,6 @@ class AlienInvasion:
 
             # Hide mouse coursor.
             pygame.mouse.set_visible(False)
-
 
     def _check_keydown_events(self, event):
         """Respond to keypresses"""
@@ -157,6 +157,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _create_fleet(self):
         """Create fleet of aliens."""
